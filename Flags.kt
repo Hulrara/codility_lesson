f49cfb3445d23708618181d4d7f3ee0f5bfccf10
김태훈 <taehoon2852@gmail.com>
2020-07-22 23:33:21 +0900

    Lesson 10-2

--- Flags.kt
+++ Flags.kt
@@ -1,7 +1,3 @@
-fun main() {
-    println(Flags().solution2(intArrayOf(1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2)))
-}
-
 class Flags {
     fun solution(A: IntArray): Int {
         var answer = 0
@@ -39,48 +35,5 @@ class Flags {
             }
         }
         return answer
-    }
-
-    fun solution2(A: IntArray): Int {
-        if (A.size < 3)
-            return 0
-
-        val Peaks = ArrayList<Int>()
-        for (i in 1 until A.size - 1) {
-            if (A[i] > A[i - 1] && A[i] > A[i + 1])
-                Peaks.add(i)
-        }
-
-        if (Peaks.size < 3)
-            return Peaks.size
-
-        var output = 0
-
-        for (K in 2..Peaks.size) {
-
-            var flags = 1
-            var peak = 0
-            var next_peak = 1
-            println()
-            while (peak < Peaks.size - 1) {
-                if (Math.abs(Peaks[peak] - Peaks[next_peak]) >= K) {
-                    flags += 1
-                    println("  ${Peaks[peak]} ${Peaks[next_peak]}")
-                    peak = next_peak
-                    next_peak = peak + 1
-
-                } else
-                    next_peak += 1
-                if (next_peak == Peaks.size || flags == K)
-                    break
-            }
-            if (output > flags) {
-                println("  $K")
-                return output
-            }
-
-            output = Math.max(flags, output)
-        }
-        return output
     }
 }
