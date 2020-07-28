b6996327abc42782376241798d634a5b0e610062
김태훈 <taehoon2852@gmail.com>
2020-07-28 23:46:49 +0900

    Lesson 12-2

--- CommonPrimeDivisors.kt
+++ CommonPrimeDivisors.kt
@@ -1,28 +1,44 @@
+fun main() {
+    print(
+        CommonPrimeDivisors().solution(
+            intArrayOf(2, 1, 2),
+            intArrayOf(1, 2, 2)
+        )
+    )
+}
+//한번더 확인
 class CommonPrimeDivisors {
     fun solution(A: IntArray, B: IntArray): Int {
-
+        var answer = 0
         for (i in A.indices) {
-            val a = A[i]
-            val b = B[i]
+            var a = A[i]
+            var b = B[i]
 
-            val GCD = gcd(a, b)
-            val check = a * b / GCD
-            val max = Math.max(a,b)
-            if ( max > check ) {
-                val div = max/check
-                if (b%div != 0){
-                    continue
-                }
+            val gcd = GCD(a, b)
+
+            var gcdA = 0
+            var gcdB = 0
+
+            while (gcdA != 1) {
+                gcdA = GCD(a, gcd)
+                a /= gcdA
             }
-            return  i
+            while (gcdB != 1) {
+                gcdB = GCD(b, gcd)
+                b /= gcdB
+            }
+            if (a == 1 && b == 1) {
+                answer += 1
+            }
+
         }
-        return -1
+        return answer
     }
 
-    fun gcd(a: Int, b: Int): Int {
+    fun GCD(a: Int, b: Int): Int {
         if (b == 0) {
             return a
         }
-        return gcd(b, a % b)
+        return GCD(b, a % b)
     }
 }
